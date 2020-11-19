d6f1d11a6fcea39e9fdbcdd86bf9da46d8c3c0dd
Opilane TTHK <opilane@tthk.ee>
2020-11-19 13:02:29 +0200

    ClearFolder

---
+++ CreateFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace CreateFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            //CreateNewFile();
+            Console.WriteLine("enter the file name: ");
+            string userInput = Console.ReadLine();
+            //CreateUserFile(userInput);
+            CreateFileWithExtension(userInput);
+
+        }
+
+        public static void CreateNewFile()
+        {
+            string rootPath = @"C:\Users\opilane\Samples\playerOne";
+            string fileName = "Balls.txt";
+            File.Create(Path.Combine(rootPath, fileName));
+        }
+
+        public static void CreateUserFile(string fileName)
+        {
+            string rootPath = @"C:\Users\opilane\Samples\playerTwo";
+            File.Create(Path.Combine(rootPath, fileName));
+        }
+        public static void CreateFileWithExtension(string fileName)
+        {
+            string rootPath = @"C:\Users\opilane\Samples\playerTwo";
+            string fullfileName = $"{fileName}.txt";
+            File.Create(Path.Combine(rootPath, fullfileName));
+        }
+    }
+}
---
+++ MoveFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace MoveFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            MoveFile();
+
+        }
+        public static void MoveFile()
+        {
+            string sourceDirectory = @"C:\Users\opilane\Samples\playerOne";
+            string destinationPath = @"C:\Users\opilane\Samples\playerTwo";
+            string fileName = "Balls.txt";
+
+            File.Move(Path.Combine(sourceDirectory, fileName), Path.Combine(destinationPath, fileName));
+        }
+    }
+}
